be472f3083ecb8fd05c9514975c916de201dc47f
sangpham97 <wunha1997@gmail.com>
2021-07-10 13:24:48 +0700

    Navbar Footer links

---
+++ src/components/contrast/links.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "gatsby"
+export const links = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "About",
+    link: "/about",
+  },
+  {
+    id: 3,
+    name: "Contact",
+    link: "/Contact",
+  },
+]
+
+export const NavList = () => {
+  return (
+    <ul className="navbar-nav  mb-2 mx-md-2 text-uppercase">
+      {links.map(item => {
+        const { id, name, link } = item
+        return <NavLink id={id} name={name} link={link} />
+      })}
+    </ul>
+  )
+}
+
+export const NavLink = ({ id, name, link }) => {
+  return (
+    <li className="nav-item" key={id}>
+      <Link className="nav-link " aria-current="page" href="#" to={link}>
+        {name}
+      </Link>
+    </li>
+  )
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,41 @@
-import * as React from "react"
-import PropTypes from "prop-types"
+import React from "react"
+import logo from "../images/gatsby-icon.png"
 import { Link } from "gatsby"
-
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+import { NavList } from "./contrast/links"
+const Header = () => (
+  <nav className="navbar navbar-expand-sm navbar-dark  bg-dark">
+    <div className="container">
+      <Link className="navbar-brand" href="/">
+        <img src={logo} alt="" className="logo-brand" />
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarCollapse">
+        <div className="mx-auto"></div>
+        <NavList />
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
-  </header>
+  </nav>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
